9b288814480b7279256e447d9a1ed40d5036bf43
Jannati Urfa Muhayat <162418616+urfayyaa@users.noreply.github.com>
2024-12-21 00:20:01 +0700

    nama pt 2

--- GameMain.java
+++ GameMain.java
@@ -1,8 +1,19 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026221161 - Ryan Adi Putra Pratama
+ * 2 - 5026231185 - Jannati Urfa Muhayat
+ * 3 - 5026231226 - Vivi Alvianita
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class GameMain extends JPanel {
+    
     private static final long serialVersionUID = 1L;
     private AIPlayer aiPlayer;
     private Timer aiTimer, humanTimer;  // Menambahkan deklarasi humanTimer
--- State.java
+++ State.java
@@ -1,4 +1,14 @@
 /**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026221161 - Ryan Adi Putra Pratama
+ * 2 - 5026231185 - Jannati Urfa Muhayat
+ * 3 - 5026231226 - Vivi Alvianita
+ */
+
+/**
  *  The enum State contains the various game states of the TTT game
  */
 public enum State {  // to save as "State.java"
