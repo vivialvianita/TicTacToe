4eab4192fe16c9f719307fa3ef280c3bc3277729
Jannati Urfa Muhayat <162418616+urfayyaa@users.noreply.github.com>
2024-12-20 23:59:01 +0700

    nama

--- Board.java
+++ Board.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026221161 - Ryan Adi Putra Pratama
+ * 2 - 5026231185 - Jannati Urfa Muhayat
+ * 3 - 5026231226 - Vivi Alvianita
+ */
+
 import java.awt.*;
 /**
  * The Board class models the ROWS-by-COLS game board.
